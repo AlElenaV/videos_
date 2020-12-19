4696b83463e429fe739c59f0b94e4dd1eaca97c5
Alena <go_getterr@yahoo.com>
2020-12-20 10:54:38 +1100

    create video-hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 
-class SearchBar extends React.Component {
-  state = { term: "" }
-
-  onInputChange = event => {
-    this.setState({ term: event.target.value })
-  }
-
-  onFormSubmit = event => {
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("")
+  const onSubmit = event => {
     event.preventDefault()
-
-    this.props.onFormSubmit(this.state.term)
+    onFormSubmit(term)
   }
-
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Video Search</label>
-            <input type="text" value={this.state.term} onChange={this.onInputChange} placeholder="Add text to search and press Enter key" />
-          </div>
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={event => setTerm(event.target.value)} placeholder="Add text to search and press Enter key" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default SearchBar
